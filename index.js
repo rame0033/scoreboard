05da8cd0fc939a86acd8cf7fbac0b14d72e491d3
Gian Ramelb <rame0033@algonquinlive.com>
2024-06-19 20:38:36 -0400

    Add html, css and js file

---
+++ index.js
@@ -0,0 +1,46 @@
+let guestScore = document.getElementById("guestEl")
+let homeScore = document.getElementById("homeEl")
+
+let homeSco = 0
+let guestSco = 0
+
+// BUTTONS FOR HOME SCOREBOARD
+function hom3add1(){
+    homeSco += 1
+    homeScore.innerText = homeSco
+}
+
+function hom3add2(){
+    homeSco += 2
+    homeScore.innerText = homeSco
+}
+
+function hom3add3(){
+    homeSco += 3
+    homeScore.innerText = homeSco
+}
+
+// BUTTONS FOR GUEST SCOREBOARD
+function guesTadd1(){
+    guestSco += 1
+    guestScore.innerText = guestSco
+}
+
+function guesTadd2(){
+    guestSco += 2
+    guestScore.innerText = guestSco
+}
+
+function guesTadd3(){
+    guestSco += 3
+    guestScore.innerText = guestSco
+}
+
+
+// Reset the game when "New Game" btn is being pressed
+function reSet(){
+    homeSco = 0
+    homeScore.innerText = homeSco
+    guestSco = 0
+    guestScore.innerText = guestSco
+}
