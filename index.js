1a1819e2af4f9ebf5ed0e83d6c0ea1f1f98c7867
Gian Ramelb <rame0033@algonquinlive.com>
2024-07-28 11:16:25 -0400

    Add log message for scoring and reset

--- index.js
+++ index.js
@@ -8,32 +8,38 @@ let guestSco = 0
 function hom3add1(){
     homeSco += 1
     homeScore.innerText = homeSco
+    console.log("Home score: " + homeSco)
 }
 
 function hom3add2(){
     homeSco += 2
     homeScore.innerText = homeSco
+    console.log("Home score: " + homeSco)
 }
 
 function hom3add3(){
     homeSco += 3
     homeScore.innerText = homeSco
+    console.log("Home score: " + homeSco)
 }
 
 // BUTTONS FOR GUEST SCOREBOARD
 function guesTadd1(){
     guestSco += 1
     guestScore.innerText = guestSco
+    console.log("Guest score: " + guestSco)
 }
 
 function guesTadd2(){
     guestSco += 2
     guestScore.innerText = guestSco
+    console.log("Guest score: " + guestSco)
 }
 
 function guesTadd3(){
     guestSco += 3
     guestScore.innerText = guestSco
+    console.log("Guest score: " + guestSco)
 }
 
 
@@ -43,4 +49,5 @@ function reSet(){
     homeScore.innerText = homeSco
     guestSco = 0
     guestScore.innerText = guestSco
+    console.log("Game reset")
 }
